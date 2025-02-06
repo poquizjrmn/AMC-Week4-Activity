2dfb16f96d2fc7c49237f27d8d12a26ad5638d74
poquizjrmn <poquizjermaine@gmail.com>
2025-02-06 16:43:40 +0800

    FlatList-Selectable

---
+++ Activity 5/App.tsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import {
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+type ItemData = {
+  id: string;
+  title: string;
+};
+
+const DATA = [
+  {
+    title: 'Morning',
+    data: ['fixing the bed', 'drinking coffee', 'showering', 'jogging with friends', 'cooking breakfast', 'eating breakfast', 'dancing'],
+  },
+  {
+    title: 'Afternoon',
+    data: ['doing assignment', 'taking online class', 'singing', 'washing dishes', 'meeting with friends', 'talking with my online friends', ],
+  },
+  {
+    title: 'Evening',
+    data: ['drinking water', 'eating hapunan', 'drinking alcohol', 'playing online games', 'watching kdrama', 'talking with my mom', 'laughing with my brother'],
+  },
+  {
+    title: 'Midnight',
+    data: ['eating midnight snacks', 'walktrip with my friends', 'drinking again with my friends', 'buying alcohol with friends', 'sleeping'],
+  },
+];
+type ItemProps = {
+  item: ItemData;
+  onPress: () => void;
+  backgroundColor: string;
+  textColor: string;
+};
+
+const Item = ({item, onPress, backgroundColor, textColor}: ItemProps) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
+    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+const App = () => {
+  const [selectedId, setSelectedId] = useState<string>();
+
+  const renderItem = ({item}: {item: ItemData}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          extraData={selectedId}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default App;
